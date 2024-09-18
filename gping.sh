15b90e4e5f3156f65feb94f2b2a6f3c890589515
subhrendu1987 <subhrendu@idrbt.ac.in>
2024-09-18 08:35:47 +0530

    modified:   gping.sh

--- gping.sh
+++ gping.sh
@@ -1,5 +1,49 @@
 #!/bin/bash
 
+# Function to convert an IP address to binary
+ip_to_binary() {
+    local a b c d
+    IFS=. read -r a b c d <<< "$1"
+    printf "%08d.%08d.%08d.%08d\n" \
+        "$(bc <<< "obase=2; $a")" \
+        "$(bc <<< "obase=2; $b")" \
+        "$(bc <<< "obase=2; $c")" \
+        "$(bc <<< "obase=2; $d")"
+}
+
+binary_to_ip() {
+    local a b c d
+    IFS=. read -r a b c d <<< "$1"
+    echo "$((2#$a)).$((2#$b)).$((2#$c)).$((2#$d))"
+}
+
+IP_and(){
+    local a1 b1 c1 d1 a2 b2 c2 d2
+    IFS=. read -r a1 b1 c1 d1 <<< "$1"
+    IFS=. read -r a2 b2 c2 d2 <<< "$2"
+    a=$(binary_and $a1 $a2)
+    b=$(binary_and $b1 $b2)
+    c=$(binary_and $c1 $c2)
+    d=$(binary_and $d1 $d2)
+    echo $a.$b.$c.$d
+}
+
+binary_and() {
+    local bin1=$1
+    local bin2=$2
+
+    # Convert binary strings to integers
+    local int1=$((2#$bin1))
+    local int2=$((2#$bin2))
+
+    # Perform binary AND operation
+    local and_result=$((int1 & int2))
+
+    # Convert the result back to binary
+    echo "obase=2; $and_result" | bc
+}
+
+
 # Function to convert dotted decimal to integer
 ip_to_int() {
     local a b c d
@@ -59,5 +103,14 @@ generate_ips_in_range() {
 BASE_IP=$1
 SUBNET_MASK=$2
 
+IP_BIN=$(ip_to_binary $BASE_IP)
+MASK_BIN=$(ip_to_binary $SUBNET_MASK)
+NET_BIN=$(IP_and $IP_BIN $MASK_BIN)
+NET_IP=$(binary_to_ip $NET_BIN)
 # Generate IPs in the given range
-generate_ips_in_range $BASE_IP $SUBNET_MASK
+#generate_ips_in_range $BASE_IP $SUBNET_MASK
+
+echo $IP_BIN
+echo $MASK_BIN
+echo $NET_BIN
+echo $NET_IP
